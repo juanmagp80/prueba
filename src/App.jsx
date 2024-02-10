0c5fab3af48bf1f953b029dd35504a3ffa40aa11
Juanma <juanmagp26@gmail.com>
2024-02-10 14:15:23 +0100

    he creado un h1

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,11 @@ import React from "react";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <div className="App">
+      <h1 className="title">Welcome to Vite + React!</h1>
+    </div>
+  );
 }
 
 export default App;
